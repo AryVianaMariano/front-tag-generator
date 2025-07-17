d50f9e856fce9bc080bd1f1e4b63d8785dd3380e
Ary Viana Mariano <ary.vm@hotmail.com>
2025-07-17 17:31:16 -0300

    Introduced a new RadioGroup component that wraps Radix primitives for building radio groups in the UI
    Updated each sortable item so clicking it opens a Popover containing an independent RadioGroup with three options
    
    Added the Radix Radio Group dependency to package.json to ensure the latest version is used

--- src/app/components/sortable-multiple/Item.tsx
+++ src/app/components/sortable-multiple/Item.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/components/ui/popover'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+
 export interface ItemData {
     id: string
     name: string
@@ -19,6 +26,9 @@ export function Item({ item, column, index }: ItemProps) {
         data: { column, type: 'item' },
     })
 
+    const [open, setOpen] = React.useState(false)
+    const [value, setValue] = React.useState('option1')
+
     const style = {
         transform: CSS.Transform.toString(transform),
         transition,
@@ -31,8 +41,34 @@ export function Item({ item, column, index }: ItemProps) {
     }
 
     return (
-        <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            {index + 1}. {item.name}
-        </div>
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+                    {index + 1}. {item.name}
+                </div>
+            </PopoverTrigger>
+            <PopoverContent className="w-40">
+                <RadioGroup value={value} onValueChange={setValue}>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="option1" id={`${item.id}-o1`} />
+                        <label htmlFor={`${item.id}-o1`} className="text-sm">
+                            Option 1
+                        </label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="option2" id={`${item.id}-o2`} />
+                        <label htmlFor={`${item.id}-o2`} className="text-sm">
+                            Option 2
+                        </label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="option3" id={`${item.id}-o3`} />
+                        <label htmlFor={`${item.id}-o3`} className="text-sm">
+                            Option 3
+                        </label>
+                    </div>
+                </RadioGroup>
+            </PopoverContent>
+        </Popover>
     )
 }
